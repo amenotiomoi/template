a482d9484ef6ecea3d606b5299aaf85032187096
amenotiomoi <129735622+amenotiomoi@users.noreply.github.com>
2023-09-13 20:07:48 +0800

    Update 树哈希.cpp

--- 数据结构/树哈希.cpp
+++ 数据结构/树哈希.cpp
@@ -10,5 +10,5 @@ class tree_hash {
       void addedge(int u,int v){edge[tree[0].second++]={u,v};cnt[u]++;cnt[v]++;}
       void init(int root){tree[0].second++;for(int i=1;i<=tree[0].second;++i)tree[i]={tree[i-1].first+cnt[i-1],cnt[i]};for(int i=0;i<tree[0].second-1;++i){pair<int,int>t=edge[i];tree[t.first].first[--cnt[t.first]]=t.second;tree[t.second].first[--cnt[t.second]]=t.first;}delete[]edge;delete[]cnt;dfs(1,0);}
       tree_hash(int n){cnt=new int[n+1];edge=new pair<int,int>[n];tree=new pair<int*,int>[n+1];hash=new hvalue[n+1];for(int i=0;i<=n;++i)cnt[i]=0;tree[0]={new int[n*2-1],0};}
-      ~tree_hash(){delete tree[0].first;delete[]tree;delete[]hash;}
+      ~tree_hash(){delete[]tree[0].first;delete[]tree;delete[]hash;}
 };
