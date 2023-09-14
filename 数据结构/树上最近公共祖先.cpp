b46b93633786a6ba649be863e0343718500dbd8c
amenotiomoi <129735622+amenotiomoi@users.noreply.github.com>
2023-09-14 10:29:20 +0800

    Update 树上最近公共祖先.cpp
    静态结构为什么要支持 resize？

--- 数据结构/树上最近公共祖先.cpp
+++ 数据结构/树上最近公共祖先.cpp
@@ -12,7 +12,6 @@ class TreeLCA {
       typedef pair<int,int> path;
       vector<vector<int>> tree;
       vector<int> siz,dep,fa,dfn;
-      void resize(int n){tree.clear();tree.resize(n+1);}
       void addedge(int u,int v){tree[u].push_back(v);tree[v].push_back(u);}
       void init(int Root){int n=tree.size()-1,cnt=0;for(auto i:{&siz,&dep,&fa,&lca_top,&dfn}){i->clear();i->resize(n+1);}lca_hev.clear();lca_hev.resize(n+1);lca_dfs1(Root,0,1);lca_dfs2(Root,0);for(int i=1;i<=n;++i){vector<int> r;for(int j:tree[i])if(j!=fa[i]){r.push_back(j);if(lca_hev[j]) swap(r.end()[-1],r[0]);}if(fa[i]!=0)r.push_back(fa[i]);tree[i]=r;}dfn_dfs(Root,0,cnt);}
       int lca(int u,int v){while(lca_top[u]!=lca_top[v]){if(dep[jump(u)]<dep[jump(v)])v=jump(v);else u=jump(u);}return(dep[u]<dep[v]?u:v);}
@@ -22,5 +21,5 @@ class TreeLCA {
       path inter(path a,path b){array<int,4>x{lca(a.first,b.first),lca(a.first,b.second),lca(a.second,b.first),lca(a.second,b.second)};sort(x.begin(),x.end(),[this](int x,int y)->int{return dep[x]>dep[y];});if(x[0]!=x[1]||dep[x[0]]==max(dep[lca(a.first,a.second)],dep[lca(b.first,b.second)]))return{x[0],x[1]};return{-1,-1};}
       path diam(){int dep1=0,idx1=0,dep2=0,idx2=0;diam_dfs(1,0,0,dep1,idx1);diam_dfs(idx1,0,0,dep2,idx2);return{idx1,idx2};}
       pair<int,int> gravity_center(){int n=tree.size()-1;vector<int>sz(n+1),we(n+1),ct;gc_dfs(1,0,sz,we,ct);ct.push_back(0);return{ct[0],ct[1]};}
-      TreeLCA(int n){resize(n);}TreeLCA(){}
+      TreeLCA(int n){tree.resize(n+1);}TreeLCA(){}
 };
