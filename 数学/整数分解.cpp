75aeeddb7f1ac5810b06469801e605db329bbdb0
amenotiomoi <129735622+amenotiomoi@users.noreply.github.com>
2023-10-08 08:43:32 +0800

    Update 整数分解.cpp

--- 数学/整数分解.cpp
+++ 数学/整数分解.cpp
@@ -6,5 +6,5 @@ namespace PR {
    ll PR(ll x){ll y=1ll*rand()*rand()%x+1,v0=1ll*rand()*rand()%(x-1)+1,v=v0,d,s=1;int t=0,k=1;while(1){v=((__int128)v*v+y)%x;s=(__int128)s*(max(v,v0)-min(v,v0))%x;if(v==v0||!s)return x;if(++t==k){if((d=__gcd(s,x))^1)return d;v0=v,k<<=1;}}}
    ll NonTrivialFact(ll x){if(Prime(x)) return -1;ll answer=1;while(answer==1||answer==x)answer=PR(x);return answer;}
    void dfs(ll x,vector<ll>& answer){if(Prime(x)){answer.push_back(x);return;}ll t=NonTrivialFact(x);dfs(t,answer);dfs(x/t,answer);}
-   vector<ll> PrimeFacList(ll x){vector<ll> answer;dfs(x,answer);return answer;}
+   vector<ll> PrimeFacList(ll x){if(x==1)return{};vector<ll> answer;dfs(x,answer);return answer;}
 };
