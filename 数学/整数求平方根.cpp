02020578b67ab31f7d5c0dfbd88fee7c3adf63d5
amenotiomoi <129735622+amenotiomoi@users.noreply.github.com>
2023-10-11 11:27:05 +0800

    Delete 数学 directory

--- 数学/整数求平方根.cpp
+++
@@ -1,1 +0,0 @@
-long long isqrt(ll x){long long a=1ll<<(__lg(x+1)/2+2),b=1ll<<(__lg(x+1)/2+1);while(a>b){a=b;b=(b+x/b)/2;}return a;}
--- 数学/线性筛.cpp
+++
@@ -1,7 +0,0 @@
-namespace fact {
-   const int n=1000000;bitset<n+2> isPrime;int OneFac[n+2];vector<int> Prime;
-   void Fac_dfs(int x,int y,vector<int>& ans,vector<pair<int,int>>& FacList){if(x==(int)(FacList.size())){ans.push_back(y);return;}int k=1;for(int i=0;i<=FacList[x].second;++i){Fac_dfs(x+1,y*k,ans,FacList);k*=FacList[x].first;}}
-   vector<pair<int,int>>GetPrimeFac(int a){if(a==1)return{};vector<pair<int,int>>answer;if(isPrime[a])return vector<pair<int,int>>{pair<int,int>{a,1}};answer.push_back({OneFac[a],1});a/=OneFac[a];while(!isPrime[a]){if(OneFac[a]==answer.back().first)answer.back().second++;else answer.push_back({OneFac[a],1});a/=OneFac[a];}if(a==answer.back().first)answer.back().second++;else answer.push_back({a,1});return answer;}
-   vector<int>GetFacList(int a){vector<int> ans;vector<pair<int,int>>FacList=GetPrimeFac(a);Fac_dfs(0,1,ans,FacList);return ans;}
-   void init(){for(int i=0;i<n;++i)isPrime[i]=1;for(int i=2;i<=n;i++){if(isPrime[i])Prime.push_back(i);for(int j:Prime){if(i*j>n) break;isPrime[i*j]=0;OneFac[i*j]=j;if(i%j==0)break;}}}
-};
