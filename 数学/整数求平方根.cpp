de8a1aa88392fefd93de8fb1c9ad14f78ebed600
amenotiomoi <129735622+amenotiomoi@users.noreply.github.com>
2023-10-11 11:23:29 +0800

    Update 整数求平方根.cpp

--- 数学/整数求平方根.cpp
+++ 数学/整数求平方根.cpp
@@ -1,1 +1,1 @@
-long long isqrt(long long x){long long a=x,b=(x+1)/2;while(a>b){a=b;b=(b+x/b)/2;}return a;}
+long long isqrt(ll x){long long a=1ll<<(__lg(x+1)/2+2),b=1ll<<(__lg(x+1)/2+1);while(a>b){a=b;b=(b+x/b)/2;}return a;}
