91d963ec61c72b1208c71080d246cd8366813b44
amenotiomoi <129735622+amenotiomoi@users.noreply.github.com>
2025-02-19 11:33:09 -0500

    Update ds_seg1.cpp

--- template/ds_seg1.cpp
+++ template/ds_seg1.cpp
@@ -4,8 +4,8 @@ template<typename node> class segment1 {
    private:
       int l,z;vector<node> t;
       struct p{segment1& segTree;int d;p(segment1& st,int i):segTree(st),d(i){}operator node()const{return segTree.t[d];}p& operator=(const node& val){segTree.change(d,val);return *this;}};
-      void change(int x,node y){x+=z;t[x]=y;for(int i=x/2;i;i/=2)t[i]=node::merge(t[i*2],t[i*2+1]);}
    public:
+      void change(int x,node y){x+=z;t[x]=y;for(int i=x/2;i;i/=2)t[i]=node::merge(t[i*2],t[i*2+1]);}void change(int x,node y){x+=z;t[x]=y;for(int i=x/2;i;i/=2)t[i]=node::merge(t[i*2],t[i*2+1]);}
       node query(int l,int r){node s[2]={};bool e[2]={true,true};for(l+=z-1,r+=z+1;(l^r)!=1;l>>=1,r>>=1){if(~l&1){s[0]=e[0]?t[l^1]:node::merge(s[0],t[l^1]);e[0]=false;}if(r&1){s[1]=e[1]?t[r^1]:node::merge(t[r^1],s[1]);e[1]=false;}}return e[0]?s[1]:e[1]?s[0]:node::merge(s[0],s[1]);}
       p operator[](int d){return p(*this,d);}
       segment1(int n){l=n;z=(1<<(__lg(n)+1));t.resize(z+n+2);for(int i=(l+z)/2+1;i<=z+n+1;++i)t[i]=node();for(int i=(l+z)/2;i>=1;--i)t[i]=node::merge(t[i*2],t[i*2+1]);}
